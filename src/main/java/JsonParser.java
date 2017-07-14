14212aac17779dbf485cce28e2d4a4458857895f
fmoghaddam <f.bakhshandegan@gmail.com>
2017-07-14 16:09:18 +0200

    added aggregated files file and folder creation

--- src/main/java/JsonParser.java
+++ src/main/java/JsonParser.java
@@ -1,6 +1,9 @@
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -13,37 +16,38 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 public class JsonParser {
-	
+
 	private static Map<String,Set<String>> labelMap = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
 	private static Map<String,Map<String,String>> linkMap = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
-	
+
 	public static void parseResultToMap(final String result){
 		labelMap.clear();
+		linkMap.clear();
 		final JSONObject obj = new JSONObject(result);
 		final JSONObject pageName = obj.getJSONObject("results");
 
 		final JSONArray arr = pageName.getJSONArray("bindings");
-		
+
 		for (int i = 0; i < arr.length(); i++) {
 			final String roleLabel = arr.getJSONObject(i).getJSONObject("roleLabel").getString("value");		
 			final String classLabel = arr.getJSONObject(i).getJSONObject("classLabel").getString("value");
 			final String roleWikipediaLink =  arr.getJSONObject(i).getJSONObject("roleWikipediaLink").getString("value");;
-			
+
 			String classAltLabel = "";
 			String roleAltLabel = "";
-			
+
 			try{
 				classAltLabel = arr.getJSONObject(i).getJSONObject("classAltLabel").getString("value");
 				roleAltLabel = arr.getJSONObject(i).getJSONObject("roleAltLabel").getString("value");
 			}catch (JSONException e) {				
 			}
-			
+
 			addToRoleMap(roleLabel,classLabel,roleAltLabel,classAltLabel);
 			addToLinkMap(roleLabel,classLabel,roleWikipediaLink);
 		}
-		
+
 	}
-	
+
 	private static void addToLinkMap(String roleLabel, String classLabel, String roleWikipediaLink) {
 		if(classLabel==null || classLabel.isEmpty() ||roleWikipediaLink==null || roleWikipediaLink.isEmpty() ){
 			return;
@@ -64,19 +68,60 @@ public class JsonParser {
 		System.err.println("Number of Category for "+folderName+" = "+labelMap.size());
 		for(Entry<String, Set<String>> a:labelMap.entrySet()){
 			try{
-			    PrintWriter writer = new PrintWriter(folderName+File.separator+a.getKey(), "UTF-8");
-			    for(String s:a.getValue()){
-			    	if(s.isEmpty()){
-			    		continue;
-			    	}
-			    	writer.println(s);
-			    }
-			    writer.close();
+				PrintWriter writer = new PrintWriter(folderName+File.separator+a.getKey(), "UTF-8");
+				for(String s:a.getValue()){
+					if(s.isEmpty()){
+						continue;
+					}
+					writer.println(s);
+				}
+				writer.close();
 			} catch (IOException e) {
 			}
 		}
 	}
 	
+	/**
+	 * Put all the roles for each specific topic in just one file
+	 * @param folderName
+	 */
+	public static void aggegateAndPrintMapStatistic(String folderName) {
+		if(!createFolder(folderName)){
+			System.err.println("Can not create folder "+folderName);
+		}
+		final Set<String> aggregationSet = new HashSet<>();
+		for(Entry<String, Set<String>> a:labelMap.entrySet()){
+			for(String s:a.getValue()){
+				if(!s.isEmpty()){
+					aggregationSet.add(s);
+				}
+			}			    
+		}
+		try{
+			PrintWriter writer = new PrintWriter(folderName+File.separator+folderName.substring(folderName.lastIndexOf('/'),folderName.length()), "UTF-8");
+			for(String s:aggregationSet){
+				writer.println(s);
+			}
+			writer.close();
+		} catch (IOException e) {
+		}
+	}
+
+	private static boolean createFolder(String folderName) {
+		Path path = Paths.get(folderName);
+        if (!Files.exists(path)) {
+        	try {
+                Files.createDirectories(path);
+                return true;
+            } catch (IOException e) {
+               return false;
+            }
+        }else{
+        	return false;
+        }
+		
+	}
+
 	private static void addToRoleMap(String roleLabel, String classLabel, String roleAltLabel, String classAltLabel) {
 		if(classLabel==null || classLabel.isEmpty()){
 			return;
@@ -100,13 +145,39 @@ public class JsonParser {
 	public static void printAnchorTextData(String folderName) {
 		for(Entry<String, Map<String, String>> a:linkMap.entrySet()){
 			try{
-			    PrintWriter writer = new PrintWriter(folderName+File.separator+a.getKey(), "UTF-8");
-			    for(Entry<String, String> s:a.getValue().entrySet()){
-			    	writer.println(s.getKey()+";"+s.getValue()+";"+s.getKey());
-			    }
-			    writer.close();
+				PrintWriter writer = new PrintWriter(folderName+File.separator+a.getKey(), "UTF-8");
+				for(Entry<String, String> s:a.getValue().entrySet()){
+					writer.println(s.getKey()+";"+s.getValue()+";"+s.getKey().replaceAll(" ", "_"));
+				}
+				writer.close();
 			} catch (IOException e) {
 			}
+		}
+	}
+
+	/**
+	 * Put all the roles for each specific topic in just one file
+	 * @param folderName
+	 */
+	public static void aggegateAndPrintAnchorTextData(String folderName) {
+		if(!createFolder(folderName)){
+			System.err.println("Can not create folder "+folderName);
+		}
+		final Set<String> aggregationSet = new HashSet<>();
+		for(Entry<String, Map<String, String>> a:linkMap.entrySet()){
+			for(Entry<String, String> s:a.getValue().entrySet()){
+				final StringBuilder result = new StringBuilder();
+				result.append(s.getKey()).append(";").append(s.getValue()).append(";").append(s.getKey().replaceAll(" ", "_"));
+				aggregationSet.add(result.toString());
+			}			    
+		}
+		try{
+			PrintWriter writer = new PrintWriter(folderName+File.separator+folderName.substring(folderName.lastIndexOf('/'),folderName.length()), "UTF-8");
+			for(String s:aggregationSet){
+				writer.println(s);
+			}
+			writer.close();
+		} catch (IOException e) {
 		}
 	}
 
--- src/main/java/RunRequest.java
+++ src/main/java/RunRequest.java
@@ -3,24 +3,59 @@ public class RunRequest {
 	public static void main(String[] args) {
 		final Request managerRequest = new Request();
 		managerRequest.setQuery(
-				"SELECT%20%20DISTINCT%20%3FroleLabel%20%3FroleAltLabel%20%20%3FclassLabel%20%3FclassAltLabel%20%3FroleWikipediaLink%20WHERE%20%7B%20%20%20%20%0A%20%20%3Frole%20wdt%3AP31%2a%7Cwdt%3AP279%2a%20wd%3AQ2462658%20.%0A%20%20%3Frole%20rdfs%3Alabel%20%3FroleLabel.%0A%20%20%0A%20%20%3Frole%20wdt%3AP279%20%3Fclass%20.%0A%20%20%3Fclass%20rdfs%3Alabel%20%3FclassLabel%20.%0A%20%20%0A%20%20OPTIONAL%7B%3Frole%20skos%3AaltLabel%2a%20%3FroleAltLabel%0A%20%20%20%20%20%20%20%20%20%20FILTER%20%28LANG%28%3FroleAltLabel%29%3D%22en%22%29%20.%0A%20%20%20%20%20%20%20%20%20%20%7D%20%23position%20has%20an%20alias%0A%20%20OPTIONAL%7B%3Fclass%20skos%3AaltLabel%2a%20%3FclassAltLabel%0A%20%20%20%20%20%20%20%20%20%20FILTER%20%28LANG%28%3FclassAltLabel%29%3D%22en%22%29%20.%0A%20%20%20%20%20%20%20%20%20%20%7D%20%23position%20has%20an%20alias%20%20%0A%20%0A%20%20%20%3FroleWikipediaLink%20schema%3Aabout%20%3Frole%20.%0A%20%20%20FILTER%20REGEX%28STR%28%3FroleWikipediaLink%29%2C%20%22en.wikipedia.org%2Fwiki%2F%22%29%20.%0A%20%20%0A%20%20filter%20langMatches%28lang%28%3FroleLabel%29%2C%22en%22%29%0A%20%20filter%20langMatches%28lang%28%3FclassLabel%29%2C%22en%22%29%0A%20%20filter%20langMatches%28lang%28%3FclassAltLabel%29%2C%22en%22%29%0A%20%20%0A%7D%20ORDER%20BY%20%3FroleLabel");
+				"SELECT%20%20DISTINCT%20%3FroleLabel%20%3FroleAltLabel%20%20%3FclassLabel%20%3FclassAltLabel%20%3FroleWikipediaLink%20WHERE%20%7B%20%20%20%20%0A%20%20%3Frole%20wdt%3AP31%2a%7Cwdt%3AP279%2a%20wd%3AQ25713832%20.%0A%20%20%3Frole%20rdfs%3Alabel%20%3FroleLabel.%0A%20%20%0A%20%20%3Frole%20wdt%3AP279%20%3Fclass%20.%0A%20%20%3Fclass%20rdfs%3Alabel%20%3FclassLabel%20.%0A%20%20%0A%20%20OPTIONAL%7B%3Frole%20skos%3AaltLabel%2a%20%3FroleAltLabel%0A%20%20%20%20%20%20%20%20%20%20FILTER%20%28LANG%28%3FroleAltLabel%29%3D%22en%22%29%20.%0A%20%20%20%20%20%20%20%20%20%20%7D%20%23position%20has%20an%20alias%0A%20%20OPTIONAL%7B%3Fclass%20skos%3AaltLabel%2a%20%3FclassAltLabel%0A%20%20%20%20%20%20%20%20%20%20FILTER%20%28LANG%28%3FclassAltLabel%29%3D%22en%22%29%20.%0A%20%20%20%20%20%20%20%20%20%20%7D%20%23position%20has%20an%20alias%20%20%0A%20%0A%20%20%20%3FroleWikipediaLink%20schema%3Aabout%20%3Frole%20.%0A%20%20%20FILTER%20REGEX%28STR%28%3FroleWikipediaLink%29%2C%20%22en.wikipedia.org%2Fwiki%2F%22%29%20.%0A%20%20%0A%20%20filter%20langMatches%28lang%28%3FroleLabel%29%2C%22en%22%29%0A%20%20filter%20langMatches%28lang%28%3FclassLabel%29%2C%22en%22%29%0A%20%20filter%20langMatches%28lang%28%3FclassAltLabel%29%2C%22en%22%29%0A%20%20%0A%7D%20ORDER%20BY%20%3FroleLabel");
 		managerRequest.setDataFormat(DataForomat.JSON);
 		String result = Caller.run(managerRequest);
 		JsonParser.parseResultToMap(result);
-		JsonParser.printMapStatistic("managers");
-		JsonParser.printAnchorTextData("managersLinks");
+		//JsonParser.printMapStatistic("managers");
+		JsonParser.aggegateAndPrintMapStatistic("./roles/ceo");
+		//JsonParser.printAnchorTextData("managersLinks");
+		JsonParser.aggegateAndPrintAnchorTextData("./rolesLinks/ceo");
 
-		final Request roleRequest = new Request();
-		roleRequest.setQuery(
-				"%23SELECT%20%20DISTINCT%20%3Frole%20%3FroleLabel%20%3FroleAltLabel%20%3Fclass%20%3FclassLabel%20%3FclassAltLabel%20WHERE%20%7B%0ASELECT%20%20DISTINCT%20%3FroleLabel%20%3FroleAltLabel%20%3FclassLabel%20%3FclassAltLabel%20%3FroleWikipediaLink%20WHERE%20%7B%0A%20%20%3Frole%20wdt%3AP31%2a%7Cwdt%3AP279%2a%20wd%3AQ214339%20.%0A%20%20%0A%20%20%3Frole%20wdt%3AP279%20%3Fclass%20.%0A%20%20%3Fclass%20rdfs%3Alabel%20%3FclassLabel%20.%0A%20%20%0A%20%20OPTIONAL%7B%3Frole%20skos%3AaltLabel%20%3FroleAltLabel%0A%20%20%20%20%20%20%20%20%20%20FILTER%20%28LANG%28%3FroleAltLabel%29%3D%22en%22%29%20.%0A%20%20%20%20%20%20%20%20%20%20%7D%20%23position%20has%20an%20alias%0A%20%20OPTIONAL%7B%3Fclass%20skos%3AaltLabel%20%3FclassAltLabel%0A%20%20%20%20%20%20%20%20%20%20FILTER%20%28LANG%28%3FclassAltLabel%29%3D%22en%22%29%20.%0A%20%20%20%20%20%20%20%20%20%20%7D%20%23position%20has%20an%20alias%0A%20%20%0A%20%20%20%3FroleWikipediaLink%20schema%3Aabout%20%3Frole%20.%0A%20%20%20FILTER%20REGEX%28STR%28%3FroleWikipediaLink%29%2C%20%22en.wikipedia.org%2Fwiki%2F%22%29%20.%0A%20%20%0A%20%20%3Frole%20rdfs%3Alabel%20%3FroleLabel%20.%0A%20%20filter%20langMatches%28lang%28%3FroleLabel%29%2C%22en%22%29%20%20%0A%20%20filter%20langMatches%28lang%28%3FclassLabel%29%2C%22en%22%29%20%20%0A%7D");
-		roleRequest.setDataFormat(DataForomat.JSON);
-
-		result = Caller.run(roleRequest);
-
-		JsonParser.parseResultToMap(result);
-		JsonParser.printMapStatistic("roles");
-		JsonParser.printAnchorTextData("rolesLinks");
 		
+		final Request presidentRequest = new Request();
+		presidentRequest.setQuery(
+				"SELECT%20%20DISTINCT%20%3FroleLabel%20%3FroleAltLabel%20%3FclassLabel%20%3FclassAltLabel%20%3FroleWikipediaLink%20WHERE%20%7B%0A%20%20%3Frole%20wdt%3AP31%2a%7Cwdt%3AP279%2a%20wd%3AQ30461%20.%0A%20%20%0A%20%20%3Frole%20wdt%3AP279%20%3Fclass%20.%0A%20%20%3Fclass%20rdfs%3Alabel%20%3FclassLabel%20.%0A%20%20%0A%20%20OPTIONAL%7B%3Frole%20skos%3AaltLabel%20%3FroleAltLabel%0A%20%20%20%20%20%20%20%20%20%20FILTER%20%28LANG%28%3FroleAltLabel%29%3D%22en%22%29%20.%0A%20%20%20%20%20%20%20%20%20%20%7D%20%23position%20has%20an%20alias%0A%20%20OPTIONAL%7B%3Fclass%20skos%3AaltLabel%20%3FclassAltLabel%0A%20%20%20%20%20%20%20%20%20%20FILTER%20%28LANG%28%3FclassAltLabel%29%3D%22en%22%29%20.%0A%20%20%20%20%20%20%20%20%20%20%7D%20%23position%20has%20an%20alias%0A%20%20%0A%20%20%20%3FroleWikipediaLink%20schema%3Aabout%20%3Frole%20.%0A%20%20%20FILTER%20REGEX%28STR%28%3FroleWikipediaLink%29%2C%20%22en.wikipedia.org%2Fwiki%2F%22%29%20.%0A%20%20%0A%20%20%3Frole%20rdfs%3Alabel%20%3FroleLabel%20.%0A%20%20filter%20langMatches%28lang%28%3FroleLabel%29%2C%22en%22%29%20%20%0A%20%20filter%20langMatches%28lang%28%3FclassLabel%29%2C%22en%22%29%20%20%0A%7D");
+		presidentRequest.setDataFormat(DataForomat.JSON);
+		result = Caller.run(presidentRequest);
+		JsonParser.parseResultToMap(result);
+		//JsonParser.printMapStatistic("roles/president");
+		JsonParser.aggegateAndPrintMapStatistic("roles/president");
+		//JsonParser.printAnchorTextData("rolesLinks/president");
+		JsonParser.aggegateAndPrintAnchorTextData("rolesLinks/president");
+		
+		final Request chancellorRequest = new Request();
+		chancellorRequest.setQuery(
+				"SELECT%20%20DISTINCT%20%3FroleLabel%20%3FroleAltLabel%20%3FclassLabel%20%3FclassAltLabel%20%3FroleWikipediaLink%20WHERE%20%7B%0A%20%20%3Frole%20wdt%3AP31%2a%7Cwdt%3AP279%2a%20wd%3AQ373085%20.%0A%20%20%0A%20%20%3Frole%20wdt%3AP279%20%3Fclass%20.%0A%20%20%3Fclass%20rdfs%3Alabel%20%3FclassLabel%20.%0A%20%20%0A%20%20OPTIONAL%7B%3Frole%20skos%3AaltLabel%20%3FroleAltLabel%0A%20%20%20%20%20%20%20%20%20%20FILTER%20%28LANG%28%3FroleAltLabel%29%3D%22en%22%29%20.%0A%20%20%20%20%20%20%20%20%20%20%7D%20%23position%20has%20an%20alias%0A%20%20OPTIONAL%7B%3Fclass%20skos%3AaltLabel%20%3FclassAltLabel%0A%20%20%20%20%20%20%20%20%20%20FILTER%20%28LANG%28%3FclassAltLabel%29%3D%22en%22%29%20.%0A%20%20%20%20%20%20%20%20%20%20%7D%20%23position%20has%20an%20alias%0A%20%20%0A%20%20%20%3FroleWikipediaLink%20schema%3Aabout%20%3Frole%20.%0A%20%20%20FILTER%20REGEX%28STR%28%3FroleWikipediaLink%29%2C%20%22en.wikipedia.org%2Fwiki%2F%22%29%20.%0A%20%20%0A%20%20%3Frole%20rdfs%3Alabel%20%3FroleLabel%20.%0A%20%20filter%20langMatches%28lang%28%3FroleLabel%29%2C%22en%22%29%20%20%0A%20%20filter%20langMatches%28lang%28%3FclassLabel%29%2C%22en%22%29%20%20%0A%7D");
+		chancellorRequest.setDataFormat(DataForomat.JSON);
+		result = Caller.run(chancellorRequest);
+		JsonParser.parseResultToMap(result);
+		//JsonParser.printMapStatistic("roles/chancellor");
+		JsonParser.aggegateAndPrintMapStatistic("roles/chancellor");
+		//JsonParser.printAnchorTextData("rolesLinks/chancellor");
+		JsonParser.aggegateAndPrintAnchorTextData("rolesLinks/chancellor");
+		
+		final Request popeRequest = new Request();
+		popeRequest.setQuery(
+				"SELECT%20%20DISTINCT%20%3FroleLabel%20%3FroleAltLabel%20%3FclassLabel%20%3FclassAltLabel%20%3FroleWikipediaLink%20WHERE%20%7B%0A%20%20%3Frole%20wdt%3AP31%2a%7Cwdt%3AP279%2a%20wd%3AQ19546%20.%0A%20%20%0A%20%20%3Frole%20wdt%3AP279%20%3Fclass%20.%0A%20%20%3Fclass%20rdfs%3Alabel%20%3FclassLabel%20.%0A%20%20%0A%20%20OPTIONAL%7B%3Frole%20skos%3AaltLabel%20%3FroleAltLabel%0A%20%20%20%20%20%20%20%20%20%20FILTER%20%28LANG%28%3FroleAltLabel%29%3D%22en%22%29%20.%0A%20%20%20%20%20%20%20%20%20%20%7D%20%23position%20has%20an%20alias%0A%20%20OPTIONAL%7B%3Fclass%20skos%3AaltLabel%20%3FclassAltLabel%0A%20%20%20%20%20%20%20%20%20%20FILTER%20%28LANG%28%3FclassAltLabel%29%3D%22en%22%29%20.%0A%20%20%20%20%20%20%20%20%20%20%7D%20%23position%20has%20an%20alias%0A%20%20%0A%20%20%20%3FroleWikipediaLink%20schema%3Aabout%20%3Frole%20.%0A%20%20%20FILTER%20REGEX%28STR%28%3FroleWikipediaLink%29%2C%20%22en.wikipedia.org%2Fwiki%2F%22%29%20.%0A%20%20%0A%20%20%3Frole%20rdfs%3Alabel%20%3FroleLabel%20.%0A%20%20filter%20langMatches%28lang%28%3FroleLabel%29%2C%22en%22%29%20%20%0A%20%20filter%20langMatches%28lang%28%3FclassLabel%29%2C%22en%22%29%20%20%0A%7D");
+		popeRequest.setDataFormat(DataForomat.JSON);
+		result = Caller.run(popeRequest);
+		JsonParser.parseResultToMap(result);
+		//JsonParser.printMapStatistic("roles/pope");
+		JsonParser.aggegateAndPrintMapStatistic("roles/pope");
+		//JsonParser.printAnchorTextData("rolesLinks/pope");
+		JsonParser.aggegateAndPrintAnchorTextData("rolesLinks/pope");
+		
+		final Request kingRequest = new Request();
+		kingRequest.setQuery(
+				"SELECT%20%20DISTINCT%20%3FroleLabel%20%3FroleAltLabel%20%3FclassLabel%20%3FclassAltLabel%20%3FroleWikipediaLink%20WHERE%20%7B%0A%20%20%3Frole%20wdt%3AP31%2a%7Cwdt%3AP279%2a%20wd%3AQ116%20.%0A%20%20%0A%20%20%3Frole%20wdt%3AP279%20%3Fclass%20.%0A%20%20%3Fclass%20rdfs%3Alabel%20%3FclassLabel%20.%0A%20%20%0A%20%20OPTIONAL%7B%3Frole%20skos%3AaltLabel%20%3FroleAltLabel%0A%20%20%20%20%20%20%20%20%20%20FILTER%20%28LANG%28%3FroleAltLabel%29%3D%22en%22%29%20.%0A%20%20%20%20%20%20%20%20%20%20%7D%20%23position%20has%20an%20alias%0A%20%20OPTIONAL%7B%3Fclass%20skos%3AaltLabel%20%3FclassAltLabel%0A%20%20%20%20%20%20%20%20%20%20FILTER%20%28LANG%28%3FclassAltLabel%29%3D%22en%22%29%20.%0A%20%20%20%20%20%20%20%20%20%20%7D%20%23position%20has%20an%20alias%0A%20%20%0A%20%20%20%3FroleWikipediaLink%20schema%3Aabout%20%3Frole%20.%0A%20%20%20FILTER%20REGEX%28STR%28%3FroleWikipediaLink%29%2C%20%22en.wikipedia.org%2Fwiki%2F%22%29%20.%0A%20%20%0A%20%20%3Frole%20rdfs%3Alabel%20%3FroleLabel%20.%0A%20%20filter%20langMatches%28lang%28%3FroleLabel%29%2C%22en%22%29%20%20%0A%20%20filter%20langMatches%28lang%28%3FclassLabel%29%2C%22en%22%29%20%20%0A%7D");
+		kingRequest.setDataFormat(DataForomat.JSON);
+		result = Caller.run(kingRequest);
+		JsonParser.parseResultToMap(result);
+		//JsonParser.printMapStatistic("roles/king");
+		JsonParser.aggegateAndPrintMapStatistic("roles/king");
+		//JsonParser.printAnchorTextData("rolesLinks/king");
+		JsonParser.aggegateAndPrintAnchorTextData("rolesLinks/king");
 	}
 
 }
